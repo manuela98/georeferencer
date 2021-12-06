ed1debe9e87384dd77f452f5a9be01bcce1021cb
ManuelaJaramillo <manuela.jaramillo@elenas.la>
2021-12-06 14:04:01 -0500

    [M] update with OOP in scripts

--- scripts/coordinate_query.py
+++ scripts/coordinate_query.py
@@ -5,24 +5,33 @@ import pandas as pd
 # connect with database local postgreSQL
 engine = create_engine('postgresql://manuela:5961@localhost:5432/db_georeferencer')
 sql = '''SELECT  geometry AS geom, "VIA", "PLACA" FROM home_nomenclature'''
-data = gpd.GeoDataFrame.from_postgis(sql, con = engine)
+data_nomenclature = gpd.GeoDataFrame.from_postgis(sql, con = engine)
 
 # transform data via removing spaces 
-data['VIA'] =  data['VIA'].str.replace(' ', '')
+data_nomenclature['VIA'] =  data_nomenclature['VIA'].str.replace(' ', '')
 
 
-def get_coordinate(address, data = data):
-    '''Return the point given an address
-    adress: via joint separated by a space with the plate
-    example: CL80C 90A-43
-    '''
-    via = address.split(' ')[0]
-    plaque = address.split(' ')[1]
-    data_via = data[data['VIA'] == via]
-    data_plaque = data_via[data_via['PLACA'] == plaque]
-    point = data_plaque['geom'].iloc[0]
-    return point.x, point.y
+
+class Adress():
+    '''Class for an adress with properties
+    latitude, longitude, ...'''
+    def __init__(self, adress, nomenclature_data):
+        self.nomenclature_data = nomenclature_data
+        self.adress = adress
+        self.via = self.adress.split(' ')[0]
+        self.plaque = self.adress.split(' ')[1]
+        self.data_adress =  self.nomenclature_data[(self.nomenclature_data['VIA'] == self.via) 
+                                                    & (self.nomenclature_data['PLACA']== self.plaque)]
+
+    def get_coordinate(self):
+         '''Return the coordenate of the adress'''
+         self.point =  self.data_adress['geom'].iloc[0]
+         self.latitude = self.point.x
+         self.longitude = self.point.y
+         return self.longitude, self.latitude 
+        
 
 
-lat , long = get_coordinate('CL80C 90A-43')
-print(lat , long)
+
+adress_example = Adress('CL80C 90A-43', data_nomenclature)
+print(adress_example.get_coordinate())
