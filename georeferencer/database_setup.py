c94f666030dc5783cb02b489dd8e727a77a191cb
ManuelaJaramillo <manuela.jaramillo@elenas.la>
2022-01-05 14:06:29 -0500

    [M] change database name and user in setup #3

--- georeferencer/database_setup.py
+++ georeferencer/database_setup.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 import geopandas as gpd
 import psycopg2
 
-engine = create_engine('postgresql://manuela:5961@localhost:5432/db2_georeferencer')
+engine = create_engine('postgresql://gis:gis@localhost:5432/georeferencer')
 nomenclature = gpd.read_file('../tests/Nomenclatura_Domiciliaria.shp')
 nomenclature.to_postgis('home_nomenclature', engine, index = True, index_label='Index')
